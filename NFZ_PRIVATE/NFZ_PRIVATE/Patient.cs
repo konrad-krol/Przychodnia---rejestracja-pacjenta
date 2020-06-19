034d877782ed5713138c62edb151a58c96bf7c0a
Konrad Król <44756736+konrad-krol@users.noreply.github.com>
2020-06-19 22:44:19 +0200

    Create Patient.cs

---
+++ NFZ_PRIVATE/NFZ_PRIVATE/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFZ_PRIVATE
+{
+    class Patient
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Index { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+    }
+}
