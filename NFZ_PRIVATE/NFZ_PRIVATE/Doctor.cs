ff8d59a7ce7edd41a9b97ca4897dcbed7f2049df
Konrad Król <44756736+konrad-krol@users.noreply.github.com>
2020-06-19 22:44:05 +0200

    Create Doctor.cs

---
+++ NFZ_PRIVATE/NFZ_PRIVATE/Doctor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFZ_PRIVATE
+{
+    class Doctor
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Index { get; set; }
+        public string Specialization { get; set; }
+    }
+}
