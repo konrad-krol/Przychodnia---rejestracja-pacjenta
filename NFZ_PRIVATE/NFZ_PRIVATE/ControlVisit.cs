311c1efbc4032fb3965d241992f1a0f2a7c851ce
Konrad Król <44756736+konrad-krol@users.noreply.github.com>
2020-06-19 22:43:57 +0200

    Create ControlVisit.cs

---
+++ NFZ_PRIVATE/NFZ_PRIVATE/ControlVisit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFZ_PRIVATE
+{
+    class ControlVisit
+    {
+        public int VisitIndex { get; set; }
+        public int DoctorIndex { get; set; }
+        public string Specialization { get; set; }
+        public string PrivateNFZ { get; set; }
+        public DateTime DateVisit { get; set; }
+        public TimeSpan TimeVisit { get; set; }
+        public int PatientIndex { get; set; }
+    }
+}
