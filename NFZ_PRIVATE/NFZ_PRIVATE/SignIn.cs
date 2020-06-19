3f3d98bd57e89a68f7937882b560c391a750a96c
Konrad Król <44756736+konrad-krol@users.noreply.github.com>
2020-06-19 22:44:28 +0200

    Create SignIn.cs

---
+++ NFZ_PRIVATE/NFZ_PRIVATE/SignIn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NFZ_PRIVATE
+{
+    class SignIn
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int PatientIndex { get; set; }
+    }
+}
